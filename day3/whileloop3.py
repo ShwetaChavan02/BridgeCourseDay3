433e9bf2e0596195fb303185b67a27a52ef6ece9
ShwetaChavan02 <shwetachavan0204@gmail.com>
2026-09-15 10:03:59 +0530

    while loop 3

---
+++ day3/whileloop3.py
@@ -0,0 +1,11 @@
+#multiplication table using while loop
+
+'''i = 0
+while i < 10:
+    print(i)
+    i += 1'''
+i = 1
+num = int(input("Enter a number: "))
+while i <= 10:
+    print(num, "x", i, "=", num*i)
+    i += 1
